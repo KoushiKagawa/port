39cfb1191dd292cb97a70d88d490d14f676c1ca9
KoushiKagawa <koushi.kagawa@resinc.co.jp>
2018-11-14 14:49:33 +0900

    Initial Commit

---
+++ nuxt.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'port',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    modules: [
+      '@nuxtjs/bulma'
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    css: [
+      '@fortawesome/fontawesome-free-webfonts',
+      '@fortawesome/fontawesome-free-webfonts/css/fa-brands.css',
+      '@fortawesome/fontawesome-free-webfonts/css/fa-regular.css',
+      '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css',
+    ],
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
